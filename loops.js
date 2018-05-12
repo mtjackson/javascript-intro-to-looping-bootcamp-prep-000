6ca6c76ac825575f94a828e2ca0c3b9e94220600
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-12 00:16:25 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,8 +20,8 @@ function whileLoop(n) {
 function doWhileLoop(array) {
   function maybeTrue() {
     return Math.random() >= 0.5;
-  } do {
-    --array;
-  } while (array.length > 0 && maybeTrue());
-  return array;
+    } do {
+     --array;
+    } while (array.length > 0 && maybeTrue());
+     return array;
 }
