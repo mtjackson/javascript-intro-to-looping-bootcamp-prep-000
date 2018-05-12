2e8e1ab2adea3aff0bfca72353a1fc1228b2cb7a
Mary Jackson <jackson.marytherese@gmail.com>
2018-05-12 00:11:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,6 @@ function forLoop(array) {
   for (i = 0; i <25; i++) {
     if(i === 1) {
       array.push("I am 1 strange loops.");
-      return array;      
     } else {
       array.push("I am ${i} strange loops.");
       return array;
